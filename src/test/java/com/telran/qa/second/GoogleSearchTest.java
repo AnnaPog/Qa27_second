744a5bfdbb460685899c2ea7036b7da55e8247f7
Anna Pogrebinskaya <aniapog@mail.ru>
2021-03-25 22:03:46 +0200

    Added Search Jave

--- src/test/java/com/telran/qa/second/GoogleSearchTest.java
+++ src/test/java/com/telran/qa/second/GoogleSearchTest.java
@@ -4,26 +4,52 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 public class GoogleSearchTest {
     WebDriver wd;
 
-    @Test
-    public void testGoogleSearch(){
+    @BeforeMethod
+   //@BeforeClass
+    public void setUp(){
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.manage().window().maximize();
         wd.get("https://www.google.ru");
-
-
-        wd.findElement(By.name("q")).click();
-        wd.findElement(By.name("q")).clear();
-        wd.findElement(By.name("q")).sendKeys("WebDriver"+ Keys.ENTER);
-
-        wd.findElement(By.cssSelector("[href='https://www.selenium.dev/documentation/en/webdriver/']")).click();
-
-
     }
+
+    @Test
+    public void testGoogleSearch(){
+        type(By.name("q"), "WebDriver"+ Keys.ENTER);
+        click(By.cssSelector("[href='https://www.selenium.dev/documentation/en/webdriver/']"));
+      //wd.navigate().back();
+        //Assert.
+    }
+
+    @Test
+    public void testSearchImg(){
+        type(By.name("q"), "Java"+Keys.ENTER);
+        click(By.cssSelector("[data-hveid='CAEQAw']"));
+    }
+
+    public void click(By locator){
+        wd.findElement(locator).click();
+    }
+
+    public void type(By locator, String text){
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+   //@AfterClass
+   @AfterMethod
+    public void tearDown(){
+        wd.quit();
+    }
+
+
 }
