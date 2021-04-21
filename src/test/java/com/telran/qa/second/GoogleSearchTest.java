05234d82e4778d4f9759c3a988c2f8f4cb133d25
Anna Pogrebinskaya <aniapog@mail.ru>
2021-04-21 18:18:12 +0300

    Added test project

--- src/test/java/com/telran/qa/second/GoogleSearchTest.java
+++ src/test/java/com/telran/qa/second/GoogleSearchTest.java
@@ -3,10 +3,12 @@ package com.telran.qa.second;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class GoogleSearchTest {
@@ -18,21 +20,29 @@ public class GoogleSearchTest {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wd.manage().window().maximize();
-        wd.get("https://www.google.ru");
+        wd.get("https://www.google.com");
     }
 
     @Test
     public void testGoogleSearch(){
         type(By.name("q"), "WebDriver"+ Keys.ENTER);
         click(By.cssSelector("[href='https://www.selenium.dev/documentation/en/webdriver/']"));
-      wd.navigate().back();
+        back();
         //Assert.
+    }
+
+    public void back() {
+        wd.navigate().back();
     }
 
     @Test
     public void testSearchImg(){
-        type(By.name("q"), "Java"+Keys.ENTER);
-        click(By.cssSelector("[data-hveid='CAEQAw']"));
+       String attribute = wd.findElement(By.name("q")).getAttribute("maxlength");
+        System.out.println(attribute);
+        Assert.assertEquals(attribute, "2048");
+        final List<WebElement> div= wd.findElements(By.tagName("div"));
+//        type(By.name("q"), "Java"+Keys.ENTER);
+//        click(By.cssSelector("[data-hveid='CAEQAw']"));
     }
 
     public void click(By locator){
